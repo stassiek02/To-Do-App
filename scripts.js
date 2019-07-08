db833ec1e06231c82a545347ba84666edfee48b0
Daniel <staszekdaniel5@gmail.com>
2019-07-08 12:14:14 +0200

    Added local storage

--- scripts.js
+++ scripts.js
@@ -1,49 +1,52 @@
-const input = document.querySelector('.todo__form-input');
-const form = document.querySelector('.todo__form');
-const btn = document.querySelector('.todo__form-btn');
-const list = document.querySelector('.todo__list');
+const input = document.querySelector(".todo__form-input");
+const form = document.querySelector(".todo__form");
+const btn = document.querySelector(".todo__form-btn");
+const list = document.querySelector(".todo__list");
 //const remove = docume.querySelector('.todo__list-remove');
 
+function addTask(e) {
+  e.preventDefault();
+  let task = input.value;
 
-function addTask (e){
-    e.preventDefault();
-    let task = input.value;
-    if(task !==''){
-        
-        task = input.value;
-        displayTask(task);
-       // clearInput();
-    }
-    e.target.reset();
-    
-};
-
-
-function displayTask(task){
-    list.insertAdjacentHTML('beforeend',`<li class="todo__list-item">${task}<span class="todo__list-remove">X</span></li>`);
-}
-function removeItem(e){
-    if(e.target && e.target.nodeName == "SPAN") {
-        e.target.closest('.todo__list-item').remove();
-    }
-}
-function done(e){
-    if(e.target && e.target.nodeName == 'LI'){
-        e.target.classList.contains('done') ? e.target.classList.remove('done') : e.target.classList.add('done');
-        
-    
-    }
+  if (task !== "") {
+    task = input.value;
+    displayTask(task);
+    // clearInput();
+  }
+  updateLocalStorage();
+  e.target.reset();
 }
 
-
-function updateLocalStorage(){
-   
+function displayTask(task) {
+  list.insertAdjacentHTML(
+    "beforeend",
+    `<li class="todo__list-item">${task}<span class="todo__list-remove">X</span></li>`
+  );
 }
-updateLocalStorage();
-console.log(list);
+function removeItem(e) {
+  if (e.target && e.target.nodeName == "SPAN") {
+    e.target.closest(".todo__list-item").remove();
+    updateLocalStorage();
+  }
+}
+function done(e) {
+  if (e.target && e.target.nodeName == "LI") {
+    e.target.classList.contains("done")
+      ? e.target.classList.remove("done")
+      : e.target.classList.add("done");
+  }
+}
 
-form.addEventListener('submit',addTask);
-list.addEventListener('click',removeItem);
-list.addEventListener('click',done);
+function updateLocalStorage() {
+  let listJson = list.innerHTML.toString().trim();
+  localStorage.setItem("ListTasks", listJson);
+}
+function loadFromLocalStorage() {
+  let listTasks = localStorage.getItem("ListTasks");
+  list.insertAdjacentHTML("beforeend", listTasks);
+}
+window.onload = loadFromLocalStorage;
 
-
+form.addEventListener("submit", addTask);
+list.addEventListener("click", removeItem);
+list.addEventListener("click", done);
